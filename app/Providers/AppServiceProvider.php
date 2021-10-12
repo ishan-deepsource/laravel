21cc909d5b08ed01aead4353c909644f9c41e2c7
Ishan Vyas <ishan@deepsource.io>
2021-10-12 15:59:43 +0530

    chore: testing
    Signed-off-by: Ishan Vyas <ishan@deepsource.io>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        print 'Hello';
+        print 'Hello from AppServiceProvider::boot()' . PHP_EOL;
     }
 }
